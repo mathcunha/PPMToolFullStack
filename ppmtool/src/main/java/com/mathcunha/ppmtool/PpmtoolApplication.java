ee75a4910d153e25752ea325ec409019b6be5ee9
Matheus Cunha <mathcunha@gmail.com>
2019-10-10 02:02:19 +0000

    project entity

--- ppmtool/src/main/java/com/mathcunha/ppmtool/PpmtoolApplication.java
+++ ppmtool/src/main/java/com/mathcunha/ppmtool/PpmtoolApplication.java
@@ -1,13 +1,27 @@
 package com.mathcunha.ppmtool;
 
+import com.mathcunha.ppmtool.domain.Project;
+import com.mathcunha.ppmtool.repositories.ProjectRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class PpmtoolApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PpmtoolApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demoSpecialty(ProjectRepository repository) {
+		return (args) -> {
+			Project project = new Project();
+			project.setName("test");
+			repository.save(project);
+		};
+	}
 }
---
+++ ppmtool/src/main/java/com/mathcunha/ppmtool/domain/Project.java
@@ -0,0 +1,71 @@
+package com.mathcunha.ppmtool.domain;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+public class Project {
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String identifier;
+    private String description;
+    @CreatedDate
+    private Date createdDate;
+    @LastModifiedDate
+    private Date updatedDate;
+
+    public Project() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getUpdatedDate() {
+        return updatedDate;
+    }
+
+    public void setUpdatedDate(Date updatedDate) {
+        this.updatedDate = updatedDate;
+    }
+}
