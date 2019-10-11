11ab233a77a6755df33e24bd592d0e2990c193b0
Matheus Cunha <mathcunha@gmail.com>
2019-10-11 16:55:48 +0000

    POST;GET - OK

--- ppmtool/src/test/jasmine/spec/project-spec.js
+++ ppmtool/src/test/jasmine/spec/project-spec.js
@@ -1,23 +1,79 @@
 var request = require("request");
 
-var base_url = "http://localhost:8090/api/project"
+var base_url = "http://localhost:8090/api/project";
 
-let Project = {
-  name:'',
-  identifier:'',
-  description:''
+let emptyProject = {
+  name: "",
+  identifier: "",
+  description: ""
 };
 
-describe("Projects List API Exists", function() {
-  describe("GET /projects", function() {
-    it("returns status code 200", function(done) {
-      request.post({uri:base_url, headers:{'Content-Type': 'application/json'}, body:JSON.stringify(Project)}, function(error, response, body) {
-	console.error('error:', error); // Print the error if one occurred
-        console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-        console.log('body:', body); // Print the HTML for the Google homepage.
-        expect(response.statusCode).toBe(400);
-        done();
-      });
+let Project = {
+  name: "Jasmine Test",
+  identifier: "JASM",
+  description: "Describing Jasmine Test"
+};
+
+let completeProject = {
+  id: 2,
+  name: "Jasmine Test",
+  identifier: "TEST",
+  description: "Describing Jasmine Test",
+  startDate: null,
+  endDate: null,
+  createdDate: "2019-10-11T16:32:05.000+0000",
+  updatedDate: "2019-10-11T16:32:05.000+0000",
+  version: 0
+};
+
+describe("Projects API", function() {
+  describe("Create Project", function() {
+    it("body should not be empty or null", function(done) {
+      request.post(
+        {
+          uri: base_url,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(emptyProject)
+        },
+        function(error, response, body) {
+          expect(response.statusCode).toBe(400);
+          expect(body).toMatch("required");
+          done();
+        }
+      );
+    });
+
+    it("identifier should not be empty", function(done) {
+      let data = Object.assign({}, Project, { identifier: null });
+      request.post(
+        {
+          uri: base_url,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data)
+        },
+        function(error, response, body) {
+          expect(response.statusCode).toBe(400);
+          expect(body).toMatch("identifier");
+          done();
+        }
+      );
+    });
+
+    it("identifier must use 4 to 5 characters", function(done) {
+      let data = Object.assign({}, Project, { identifier: "JASMINE" });
+
+      request.post(
+        {
+          uri: base_url,
+          headers: { "Content-Type": "application/json" },
+          body: JSON.stringify(data)
+        },
+        function(error, response, body) {
+          expect(response.statusCode).toBe(400);
+          expect(body).toMatch("identifier");
+          done();
+        }
+      );
     });
   });
 
@@ -44,12 +100,14 @@ describe("Projects List API Exists", function() {
     it("API Response should be valid array of project objects 2", function(done) {
       request.get(base_url, function(error, response, body) {
         let projects = JSON.parse(body);
-        const projectRows = projects.map((projectRow) => {
-          expect(JSON.stringify(Object.keys(Project).sort()) === JSON.stringify(Object.keys(projectRow).sort())).toBeTruthy();
+        const projectRows = projects.map(projectRow => {
+          expect(
+            JSON.stringify(Object.keys(completeProject).sort()) ===
+              JSON.stringify(Object.keys(projectRow).sort())
+          ).toBeTruthy();
         });
         done();
       });
     });
-
   });
 });
